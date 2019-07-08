70bd84a7030099bde3dd25078525bb03ccf2014d
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-08 10:32:25 -0600

    Update Datagov::Food with attr_readers and additional data_source attribute

--- app/models/datagov/food.rb
+++ app/models/datagov/food.rb
@@ -1,8 +1,15 @@
 class Datagov::Food
+  attr_reader :ndb,
+              :name,
+              :group,
+              :data_source,
+              :manufacterer
+              
   def initialize(attributes)
     @ndb = attributes[:ndbno]
     @name = attributes[:name]
     @group = attributes[:group]
+    @data_source = attributes[:ds]
     @manufacturer = attributes[:manu]
   end
 end
