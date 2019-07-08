1faf26d258ae5a88b0ff36925c3824a97b9e4428
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-08 11:12:37 -0600

    Finish DatagovService spec

--- spec/services/datagov_service.rb
+++ spec/services/datagov_service.rb
@@ -9,17 +9,16 @@ RSpec.describe DatagovService do
 
   context '#foods_search' do
     it 'returns a list of foods using search params' do
-      stub_data = File.read('./spec/fixtures/sweet_potato_results.json')
-
-      search = stub_request(:get, 'https://api.nal.usda.gov/ndb/search').to_return(status: 200, body: stub_data)
-
-      foods = search[:body]
+      foods_search = subject.foods_search
 
       expect(foods_search).to be_a(Hash)
-      expect(foods_search.count).to eq(10)
-      expect(foods_search[]).to be_a Hash
-      expect(foods_search.first).to have_key(:name)
-      expect(foods_search.first).to have_key(:html_url)
+      expect(foods_search[:list][:item].count).to eq(150)
+      expect(foods_search[:list][:item]).to be_an(Array)
+      expect(foods_search[:list][:item].first).to have_key(:ndbno)
+      expect(foods_search[:list][:item].first).to have_key(:name)
+      expect(foods_search[:list][:item].first).to have_key(:group)
+      expect(foods_search[:list][:item].first).to have_key(:ds)
+      expect(foods_search[:list][:item].first).to have_key(:manu)
     end
   end
 end
